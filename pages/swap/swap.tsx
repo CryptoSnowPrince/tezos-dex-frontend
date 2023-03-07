2fc43ec92f885d13839f137626ae7b886ba95c52
CryptoSnowPrince <topdirector2017@gmail.com>
2023-03-07 15:34:29 +0900

    update

--- pages/swap/swap.tsx
+++ pages/swap/swap.tsx
@@ -1,12 +1,9 @@
-import styles from '../../styles/pages/liquidity.module.scss'
+import styles from "../../styles/pages/swap.module.scss"
+import React, { useState, useEffect, useMemo } from "react";
 import Navbar from "../../components/navbar";
-import Pinkbtn from "../../components/buttons/pinkbtn";
-import React, { useEffect, useState, useMemo } from "react";
-import Link from "next/link";
-import CryptoSlider from "../../components/cryptoSlider";
-import Footer from "../../components/footer";
+// import SelectTokenModal from "../../components/selectTokenModal";
 import SelectToken from "../../components/selectToken";
-import { useRouter } from "next/router";
+import Setting from "../../components/setting";
 
 import clsx from "clsx";
 import { tokensList } from "../../src/constants/tokensList";
@@ -27,7 +24,7 @@ import {
 import { Chain, MigrateToken } from "../../src/config/types";
 import { tEZorCTEZtoUppercase } from "../../src/api/util/helpers";
 
-interface ILiquidityProps {
+interface ISwapProps {
     className?: string;
     otherProps: {
         connectWallet: () => void;
@@ -36,29 +33,7 @@ interface ILiquidityProps {
     };
 }
 
-export default function Liquidity(props: ILiquidityProps) {
-
-    let [step1, setStep1] = useState(true);
-    let [step2, setStep2] = useState(false);
-    let [step3, setStep3] = useState(false);
-
-    let [token1, setToken1] = useState('');
-    let [token2, setToken2] = useState('');
-
-    let [isPairsClicked, setIsPairsClicked] = useState([
-        {
-            "activated": false
-        },
-        {
-            "activated": false
-        },
-        {
-            "activated": false
-        }
-    ])
-
-    const router = useRouter();
-
+export default function SwapBody(props: ISwapProps) {
     const TOKEN = useAppSelector((state) => state.config.tokens);
     const tokenPrice = useAppSelector((state) => state.tokenPrice.tokenPrice);
     const userSettings = useAppSelector((state) =>
@@ -637,142 +612,152 @@ export default function Liquidity(props: ILiquidityProps) {
         }
     }, [tokensListConfig, allBalance.allTokensBalances]);
 
+    let [setting, setSetting] = useState(false);
+
+    let [test, setTest] = useState([
+        {
+            "name": "name1",
+            "value": "value1"
+        },
+        {
+            "name": "name2",
+            "value": "value2"
+        }
+    ]);
+
+
     return (
         <>
-            <div className={styles.liquidity}>
-
+            <div className={styles.swap}>
                 <Navbar />
-                <section className="section sec1 liquidity liquidity1 liquidity1-sec1">
+                <section className="section sec1 swap-main">
                     <div className="sec-wrapper">
                         <div className="titles">
-                            <div className="title-wrapper">
-                                <img className="pointer" src="/images/left_arrow.png" onClick={() => { router.back() }} />
-                                <h1 className="title">Add Liquidity</h1>
-                            </div>
-                            <div className="extras">
-                                <div className="more">
-                                    <p>Clear All</p>
-                                </div>
-                                <Link href="/liquidity" className="img-setting">
-                                    <img src="/images/settings.png" />
-                                </Link>
-                            </div>
+                            <h1 className="title">Swap</h1>
+                            <img className="pointer" src="/images/settings.png" onClick={() => { setSetting(!setting) }} />
+                            {
+                                setting
+                                    ?
+                                    <><Setting /></>
+                                    :
+                                    <></>
+                            }
                         </div>
                         <div className="content-box">
-                            <div className="content-part-wrapper left">
-                                <div className="content-part content-part1">
-                                    <h2 className="title">Select Pair</h2>
-                                    <SelectToken />
-                                    <SelectToken />
-                                </div>
-                                <div className={step1 ? "content-part content-part2" : "content-part content-part2 greyText"}>
-                                    <h2 className="title">Select Fee Tier</h2>
-                                    <p className="desc">The % you will earn in fee’s</p>
-                                    <div className="pairs-wrapper">
-                                        <div className={isPairsClicked[0].activated ? "pairs pairs-activated" : "pairs"} onClick={() => {
-                                            let temp = [...isPairsClicked];
-                                            temp[0].activated = !temp[0].activated;
-                                            setIsPairsClicked([...temp]);
-                                            setStep2(!step2);
-                                            for (let i = 0; i < temp.length; i++) {
-                                                if (temp[i].activated) {
-                                                    alert("no");
-                                                }
-                                            }
-                                        }}>
-                                            <div className="number">0.05%</div>
-                                            <div className="text">Best for stable pairs</div>
-                                        </div>
-                                        <div className={isPairsClicked[1].activated ? "pairs pairs-activated" : "pairs"} onClick={() => {
-                                            let temp = [...isPairsClicked];
-                                            temp[1].activated = !temp[1].activated;
-                                            setIsPairsClicked([...temp]);
-                                            setStep2(!step2);
-                                        }}>
-                                            <div className="number">0.3%</div>
-                                            <div className="text">Best for most pairs</div>
-                                        </div>
-                                        <div className={isPairsClicked[2].activated ? "pairs pairs-activated" : "pairs"} onClick={() => {
-                                            let temp = [...isPairsClicked];
-                                            temp[2].activated = !temp[2].activated;
-                                            setIsPairsClicked([...temp]);
-                                            setStep2(!step2);
-                                        }}>
-                                            <div className="number">1%</div>
-                                            <div className="text">Best for exotic pairs</div>
-                                        </div>
+                            <div className="content-box-left content-box-container">
+                                <div className="content-box-top">
+                                    <div className="content-box-number">
+                                        {test[0].name} and {test[0].value}
+                                    </div>
+                                    <div className="content-box-select">
+                                        <SelectToken />
                                     </div>
                                 </div>
-                                <div className={step2 ? "content-part content-part3" : "content-part content-part3 greyText"}>
-                                    <h2 className="title">Deposit amount</h2>
-                                    <div className="deposit-wrapper">
-                                        <div className="deposit">
-                                            <img src="/images/cryptoLogo/ada.png" className="deposit-crypto-img" />
-                                            <div className="top">
-                                                <div className="deposit-top-balance">0</div>
-                                                <p className="deposit-crypto">ETH</p>
-                                            </div>
-                                            <div className="bottom">
-                                                <div className="deposit-bottom-balance">
-                                                    Balance: 0
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="deposit">
-                                            <img src="/images/cryptoLogo/bao.png" className="deposit-crypto-img" />
-                                            <div className="top">
-                                                <div className="deposit-top-balance">0</div>
-                                                <p className="deposit-crypto">ETH</p>
-                                            </div>
-                                            <div className="bottom">
-                                                <div className="deposit-bottom-balance">
-                                                    Balance: 0
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div className="content-box-bottom">
+                                    balance :
                                 </div>
 
                             </div>
+                            <div className="content-box-mid pointer" onClick={() => {
+                                let copy = [];
+                                let arr = [];
 
-                            <div className={step3 ? "content-part-wrapper right" : "content-part-wrapper right greyText"}>
-                                <h2 className="title">Set Price Range</h2>
-                                <div className="select">
-                                    <div className="select-token">
+                                arr = [...test];
+                                copy[0] = arr[1];
+                                copy[1] = arr[0];
 
+                                setTest([...copy]);
+                            }}>
+                                <img src="/images/swap.png" />
+                            </div>
+                            <div className="content-box-right content-box-container">
+                                <div className="content-box-top">
+                                    <div className="content-box-select">
+                                        <SelectToken />
                                     </div>
-                                    <div className="select-token">
-
+                                    <div className="content-box-number">
+                                        {test[1].name} and {test[1].value}
                                     </div>
                                 </div>
+                                <div className="content-box-bottom">
+                                    balance :
+                                </div>
+
+                            </div>
+                            <div className="content-box-btn">
+                                <button className="swap-btn">Swap</button>
                             </div>
                         </div>
 
-
                     </div>
-                </section>
 
-                <Footer />
-                <SwapModal
-                    tokens={tokensListConfig.filter((e: any) => {
-                        return (
-                            e.name.toLowerCase() !== MigrateToken.KODEX.toLowerCase() &&
-                            e.name.toLowerCase() !== MigrateToken.WRAP.toLowerCase()
-                        );
-                    })}
-                    show={swapModalShow}
-                    isSuccess={allBalance.success}
-                    isLoading={allBalance.success}
-                    allBalance={allBalance.allTokensBalances}
-                    selectToken={selectToken}
-                    onhide={handleClose}
+                </section>
+            </div>
+            <div
+                className={clsx(
+                    "bg-card-500 md:border border-y border-text-800 mt-[70px] lg:mt-[75px] md:rounded-3xl  text-white lg:w-640 pt-5 pb-2 mx-auto fade-in"
+                )}
+            >
+                <SwapTab
+                    walletAddress={props.otherProps.walletAddress}
+                    firstTokenAmount={firstTokenAmount}
+                    secondTokenAmount={secondTokenAmount}
+                    connectWallet={props.otherProps.connectWallet}
                     tokenIn={tokenIn}
                     tokenOut={tokenOut}
-                    tokenType={tokenType}
-                    searchQuery={searchQuery}
-                    setSearchQuery={setSearchQuery}
+                    tokens={tokensListConfig}
+                    handleTokenType={handleTokenType}
+                    setSlippage={setSlippage}
+                    slippage={slippage}
+                    handleClose={handleClose}
+                    changeTokenLocation={changeTokenLocation}
+                    setSecondTokenAmount={setSecondTokenAmount}
+                    setFirstTokenAmount={setFirstTokenAmount}
+                    handleSwapTokenInput={handleSwapTokenInput}
+                    setTokenIn={setTokenIn}
+                    setTokenOut={setTokenOut}
+                    setTokenType={setTokenType}
+                    tokenPrice={tokenPrice}
+                    recepient={recepient}
+                    setRecepient={setRecepient}
+                    setShowConfirmSwap={setShowConfirmSwap}
+                    showConfirmSwap={showConfirmSwap}
+                    setShowConfirmTransaction={setShowConfirmTransaction}
+                    showConfirmTransaction={showConfirmTransaction}
+                    setShowTransactionSubmitModal={setShowTransactionSubmitModal}
+                    showTransactionSubmitModal={showTransactionSubmitModal}
+                    loading={loading.current}
+                    resetAllValues={resetAllValues}
+                    routeDetails={routeDetails.current}
+                    allPath={allPathState}
+                    setErrorMessage={setErrorMessage}
+                    errorMessage={errorMessage}
+                    setEnableMultiHop={setEnableMultiHop}
+                    enableMultiHop={enableMultiHop}
+                    setBalanceUpdate={setBalanceUpdate}
+                    isSwitchClicked={isSwitchClicked.current}
+                    allBalance={allBalance}
                 />
             </div>
+            <SwapModal
+                tokens={tokensListConfig.filter((e: any) => {
+                    return (
+                        e.name.toLowerCase() !== MigrateToken.KODEX.toLowerCase() &&
+                        e.name.toLowerCase() !== MigrateToken.WRAP.toLowerCase()
+                    );
+                })}
+                show={swapModalShow}
+                isSuccess={allBalance.success}
+                isLoading={allBalance.success}
+                allBalance={allBalance.allTokensBalances}
+                selectToken={selectToken}
+                onhide={handleClose}
+                tokenIn={tokenIn}
+                tokenOut={tokenOut}
+                tokenType={tokenType}
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+            />
         </>
     )
 }
